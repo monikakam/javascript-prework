cd0cbfed954b2fbe47daf499dc507e04f4b4cfd7
Monika Kamecka <monika.kamecka@gmail.com>
2020-12-18 16:37:47 +0100

    Add modiefied js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function printMessage(msg){
+/*function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
@@ -7,7 +7,10 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+*/
 
+
+/*
 printMessage('Zagralem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 printMessage('Zagralem ' + 'papier' + '! Jeśli Twój ruch to papier, to remis!');
@@ -19,13 +22,29 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to remis!');
+*/
 
-var randomNumber;
+
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
 if (randomNumber == '1' {
 	computerMove = 'kamień';
+} else {
+	computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+if (randomNumber == '2' {
+	computerMove = 'papier';
+} else {
+	computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+if (randomNumber == '3' {
+	computerMove = 'nożyce';
 } else {
 	computerMove = 'nieznany ruch';
 }
