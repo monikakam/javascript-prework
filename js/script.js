854039bf7164151c72824a6e1338a2614adfb81d
Monika Kamecka <monika.kamecka@gmail.com>
2020-12-21 18:26:36 +0100

    Add playerMove and computerMove in js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*function printMessage(msg){
+function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
@@ -7,14 +7,15 @@
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-*/
+
 
 
 /*
 printMessage('Zagralem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 printMessage('Zagralem ' + 'papier' + '! Jeśli Twój ruch to papier, to remis!');
-
+*/
+/*
 var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
@@ -24,50 +25,40 @@ playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to remis!');
 */
 
-
-var computerMove, randomNumber;
+var computerMove;
+var randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
-
-if (randomNumber == '1' {
+if (randomNumber == '1') {
 	computerMove = 'kamień';
-} else {
-	computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-
-if (randomNumber == '2' {
+} else if (randomNumber == '2') {
 	computerMove = 'papier';
-} else {
-	computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-
-if (randomNumber == '3' {
+} else if (randomNumber == '3') {
 	computerMove = 'nożyce';
 } else {
 	computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
 
-var playerMove, playerInput;
+var playerMove;
+var playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'kamień';
-	else if {
-	if (playerInput == '2') {
+	} else if (playerInput == '2') {
 		playerMove = 'papier';
 	}
-	else if {
-	if (playerInput == '3') {
+	else if (playerInput == '3') {
 		playerMove = 'nożyce';
 	}
   	else {
-  	playerMove = 'nieznany ruch';
+  	playerMove = 'błędny wybór';
 	}
-}
+
 printMessage('Twój ruch: ' + playerMove);
+
+
 
 
 
