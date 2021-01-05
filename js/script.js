27909006dbdccbfe52391d018d1830d305e59c74
Monika Kamecka <monika.kamecka@gmail.com>
2021-01-05 20:29:01 +0100

    Add modified js

--- js/script.js
+++ js/script.js
@@ -84,11 +84,11 @@ computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 
-let argButtonName;
+const argButtonName; buttonRock, buttonPaper, buttonScissors;
 
-const buttonRock = document.getElementById('button-rock');
-const buttonPaper = document.getElementById('button-paper');
-const buttonScissors = document.getElementById('button-scissors');
+buttonRock = document.getElementById('button-rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
 
 
 
